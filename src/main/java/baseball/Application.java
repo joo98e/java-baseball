f4eee691af73358485841bc27fcc58cb3406a658
Ayaan <ayaan@myfair.co>
2023-09-15 18:23:12 +0900

    git initialize

---
+++ src/main/java/baseball/Application.java
@@ -0,0 +1,10 @@
+package baseball;
+
+import baseball.scanner.InputScanner;
+
+public class Application {
+    public static void main(String[] args) {
+        String input = InputScanner.input();
+        System.out.println(input);
+    }
+}
---
+++ src/main/java/baseball/scanner/InputScanner.java
@@ -0,0 +1,12 @@
+package baseball.scanner;
+
+
+import java.util.Scanner;
+
+public class InputScanner {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String input() {
+        return scanner.next();
+    }
+}
