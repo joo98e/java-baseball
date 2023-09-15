7220e51a74b917b51f60f98fff136857d1238016
Ayaan <ayaan@myfair.co>
2023-09-15 18:24:22 +0900

    try/first: initialize

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,5 +6,6 @@ public class Application {
     public static void main(String[] args) {
         String input = InputScanner.input();
         System.out.println(input);
+        System.out.println("1");
     }
 }
